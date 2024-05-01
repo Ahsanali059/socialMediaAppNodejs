ad40ccdefabdaa30f6a362503740235f82ed0786
Ahsanali059 <AhsanDev.org@gmail.com>
2024-05-01 18:25:05 +0500

    added connection to database

---
+++ src/config/database.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery",false);
+
+
+class Database {
+    constructor(uri,options)
+    {
+        this.uri = uri;
+        this.options = options;
+    }
+
+    async connect()
+    {
+        try
+        {
+            await mongoose.connect(this.uri,this.options);
+            console.log(
+                `Connected to the Database:${mongoose.connection.db.Database}`
+            );
+        }catch(err)
+        {
+           console.log("something went wrong while conneted to database "+err)
+        }
+    }
+
+    async disconnect()
+    {
+        try
+        {
+           await mongoose.disconnect();
+           console.log(`
+           Disconnected from database:${mongoose.connection.db.Database};
+           `)
+        }
+        catch(err)
+        {
+           throw err;
+        }
+    }
+}
