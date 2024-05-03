b22803e398b66b57d4f795f9172779cdafbc59c8
Ahsanali059 <AhsanDev.org@gmail.com>
2024-05-04 00:54:24 +0500

    added pre render Hook

--- src/models/post.modol.js
+++ src/models/post.modol.js
@@ -5,6 +5,83 @@ const fs = require("fs");
 const path = require("path");
 const { promisify } = require("util");
 
+const pendingPostSchema = new Schema(
+    {
+      content: {
+        type: String,
+        trim: true,
+      },
+      fileUrl: {
+        type: String,
+        trim: true,
+      },
+      fileType: {
+        type: String,
+      },
+      community: {
+        type: Schema.Types.ObjectId,
+        ref: "Community",
+        required: true,
+      },
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      status: {
+        type: String,
+        enum: ["pending"],
+        default: "pending",
+      },
+      confirmationToken: {
+        type: String,
+        unique: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  pendingPostSchema.pre("remove", async function (next) {
+    try {
+      if (this.fileUrl) 
+      {
+        
+        const filename = path.basename(this.fileUrl);
+        const deleteFilePromise = promisify(fs.unlink)(
+          path.join(__dirname, "../assets/userFiles", filename)
+        );
+        await deleteFilePromise;
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  pendingPostSchema.pre("deleteMany", async function (next) {
+    try 
+    {
+      const pendingPosts = await this.model.find(this.getFilter());
+      for (const post of pendingPosts) {
+        if (post.fileUrl) {
+          const filename = path.basename(post.fileUrl);
+          const deleteFilePromise = promisify(fs.unlink)(
+            path.join(__dirname, "../assets/userFiles", filename)
+          );
+          await deleteFilePromise;
+        }
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  module.exports = mongoose.model("PendingPost", pendingPostSchema);
+
+
 /*
   Text Indexing: postSchema.index({ content: "text" }) creates a text index on the content field of the Post schema. This allows for efficient text-based searches on the content field.
 Pre-remove Hook: This code defines a pre-remove middleware function that will execute before a Post document is removed from the database.
